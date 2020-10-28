69abf75332690f834020476a995ab07e589fc4b6
Mudit Sharma <muditsharma23702@gmail.com>
2020-10-28 21:57:53 +0530

    Update evenOdd.cpp

--- evenOdd.cpp
+++ evenOdd.cpp
@@ -5,7 +5,7 @@ int main()
     int n;
     printf("Enter the number: ");
     scanf("%d",&n);
-    switch (n%2)
+    switch (n%2)   //checking n is even or not
     {
     case 0:
         printf("even number");
